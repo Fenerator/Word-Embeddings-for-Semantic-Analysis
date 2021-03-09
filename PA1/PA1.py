b9bb2381a02003f19e1e4106a1052e25a2c8e8e7
Francesco <francesco.tinner@uzh.ch>
2021-03-09 16:25:40 +0100

    started count_occurrences

--- PA1/PA1.py
+++ PA1/PA1.py
@@ -2,6 +2,8 @@
 
 import sys
 import string
+from collections import Counter
+
 
 def preprocessing(textfile):
     '''
@@ -31,6 +33,61 @@ def preprocessing(textfile):
 
 
 
+def get_aggregated_window_text(textfile, center_word, window_size):
+    border = window_size//2 # assuming uneven window_size
+
+    #get index positions of center word
+    center_index = []
+    for i, j in enumerate(textfile):
+        if j == center_word:
+            center_index.append(i)
+
+    #get all text for center word
+    center_text = []
+    for el in center_index: # at each occurrence of center_word:
+        center_text.append(textfile[el-border:el]) #TODO #problem wenn erster Teil segativ wird!  #lower boundary
+        center_text.append(textfile[el+1:el+border+1]) # upper boundary
+
+    return center_text
+
+
+
+
+
+
+
+
+
+
+def count_occurences(textfile, center_word, window_size, T):
+    counts = [0] * len(T)  # stores values
+    center_text = get_aggregated_window_text(textfile, center_word, window_size)
+
+    #delete all words not in T
+
+    counter(center_text)
+    return counts
+
+
+
+def TxB(textfile, B, T):
+    window_size = 5
+
+    #Preprocess textfiles B and T as well
+    B = preprocessing(B)
+    T = preprocessing(T)
+    print('T: ', T)
+    print('B: ', B)
+
+    cooccurence_matrix = {key: [0]*len(T) for key in B} #structure to store values (for each T -> 0 vector)
+
+
+
+
+
+
+
+
 
 
 
@@ -42,7 +99,17 @@ def main(arguments):
     #Step 1: Preprocessing
     text_list = preprocessing(textfile)
     print(len(text_list))
-    #Step 2:
+    #Step 2: Co-occurence matrix
+    cooccurence_matrix = TxB(textfile, B, T)
+
+
+
+
+
+
+
+
+
 
 
 
