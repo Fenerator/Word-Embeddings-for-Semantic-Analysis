b164f99a6c73f269953dea3d706c4181e6dbe5ae
Francesco <francesco.tinner@uzh.ch>
2021-03-22 20:15:59 +0100

    updated main

--- PA1/PA1.py
+++ PA1/PA1.py
@@ -230,7 +230,7 @@ def main(arguments):
 
 if __name__ == "__main__":
     if len(sys.argv) ==1:
-        #main(['text.txt', 'B.txt', 'T.txt']) # B = context words, T = center words
-        main(['text_V2.txt', 'B_V2.txt', 'T_V2.txt'])
+        #main(['text.txt', 'B.txt', 'T.txt'])
+        main(['text_V2.txt', 'B_V2.txt', 'T_V2.txt']) # B = context words, T = center words
     else:
         main(sys.argv[1:])
