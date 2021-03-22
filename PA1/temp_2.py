0fbfd0722707a2befe240119674bd5d47a5ca681
Francesco <francesco.tinner@uzh.ch>
2021-03-22 14:13:29 +0100

    added edge case

---
+++ PA1/temp_2.py
@@ -0,0 +1,10 @@
+text_list = ['wars', 'es', 'god', 'something', 'war', 'something', 'states', 'united', 'something', 'health', 'something', 'fair', 'to', 'you', 'a', 'history', 'something', 'immigrant', 'something', 'everyone', 'as', 'me', 'god', 'something', 'war', 'something', 'states', 'war']
+'''
+text_list[el-border:el]
+text_list[el+1:el+border+1] '''
+
+
+l = ['It', 'is', 'two', 'pm.']
+In[4]:  l[3:5]
+Out[4]: ['pm.']
+
