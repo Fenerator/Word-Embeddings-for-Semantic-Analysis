e4f95418f32e48a8e11bd19189b4139eab8ddf36
Francesco <francesco.tinner@uzh.ch>
2021-04-12 12:33:13 +0200

    implemented all

--- PA2/pa2.py
+++ PA2/pa2.py
@@ -2,44 +2,63 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pandas as pd
 
 # Parameters
 learning_rate = 0.2
 
-# Data
-# use bias inpus, add 1 in last coordinate of points, and add an additional weight
-training_set_nand = [([0,0,1],1),([0,1,1],1),([1,0,1],1),([1,1,1],0)]
-#training_set_nand = [([5,0,1],1),([6,3,1],1),([9,0,1],0),([-3,0,1],0)]
-weights = [0.0,0.0,0.0]
+# Data: creating input format from txt file, use bias inpus, add 1 in last coordinate of points, and add an additional weight
+df = pd.read_csv('pa2_input.txt', sep='\t')
+df = df.drop(['Word', '|'], axis=1)
+
+#create vector from labels
+text_labels = df['Label'].tolist()
+df = df.drop('Label', axis=1)
+
+# encode text_labels 1 for war, 0 for peace
+labels =  []
+for el in text_labels:
+    if el == 'WAR':
+        labels.append(1)
+    elif el == 'PEACE':
+        labels.append(0)
+    else:
+        raise KeyError
+
+#add bias (1 for each last coordinate) of points
+bias = [1] * len(labels)
+df['bias'] = bias
+
+#create vector from row of df including bias term
+points = df.values.tolist()
+
+#create datastructure [([point coordinates], label), ...]
+training_set = list(zip(points, labels))
+weights = [0.0] * len(points[0]) # initialize weights with 0
 
 
-
-def unit_step(x):
-    return 1.0* (x>=0)  # returns 1 if x >=0
-
-def decision_boundary(x):
+def decision_boundary(x): # needed later to compare whether output == label
     return 1.0* (x>=0.005)
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-for i in range(10000):
+for i in range(100000):
     error_count=0
-    for input,desired_out in training_set_nand:
-        result = sigmoid(np.dot(input,weights))
-        print("input:",input, "output:",result, 'Label: ', desired_out, "Correctly Classified: ", decision_boundary(result) == desired_out)
-        error= desired_out-result
-        print('Error ', error)
+    for point, label in training_set:
+        result = sigmoid(np.dot(point, weights))
+        #print("input:",input, "output:",result, 'Label: ', desired_out, "Correctly Classified: ", decision_boundary(result) == desired_out)
+        #print("output result:", result, 'Label: ', label, "Correctly Classified: ", decision_boundary(result) == label)
+        error = label - result
+        #print('Error ', error)
         if abs(error) > 0.005:
             error_count+=1
-            for i,val in enumerate(input):
+            for i,val in enumerate(point):
                 weights[i]+= val * error * learning_rate
+        print('Nr. of errors in this iteration: ', error_count)
     # Stopping Criterion
     if error_count==0:
         print("#" * 60)
         print('Weights: ', weights)
         print("#" * 60)
         break
-    print("#"*60)
-    print('Weights: ', weights)
-    print("#"*60)
--- PA2/temp2.py
+++ PA2/temp2.py
@@ -1,2 +1,39 @@
 #creating input format from txt file
+import pandas as pd
+df = pd.read_csv('pa2_input.txt', sep='\t')
+df = df.drop(['Word', '|'], axis=1)
+
+#create vector from labels
+text_labels = df['Label'].tolist()
+df = df.drop('Label', axis=1)
+
+# encode text_labels
+labels =  []
+for el in text_labels:
+    if el == 'WAR':
+        labels.append(1)
+    elif el == 'PEACE':
+        labels.append(0)
+    else:
+        raise KeyError
+
+#add bias (1 for each last coordinate) of points
+bias = [1] * len(labels)
+df['bias'] = bias
+
+#create vector from row of df
+points = df.values.tolist()
+
+#create datastructure [([point coordinates], label), ...]
+training_set = list(zip(points, labels))
+
+print(df)
+
+print(training_set)
+
+print(len(training_set))
+
+print(len(labels))
+
+print(len(points[0]))
 
